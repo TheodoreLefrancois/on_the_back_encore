ead6705d229eed67d310e48e024cce066497fbd4
RZucconi <romain.zucconi@gmail.com>
2021-01-13 17:00:35 +0100

    start creating media route

--- src/routes/media.js
+++ src/routes/media.js
@@ -3,4 +3,13 @@ const prisma = require('../prismaClient');
 
 const router = express.Router();
 
+router.get('/media', async (req, res, next) => {
+  try {
+    const results = await prisma.media.findMany();
+    res.status(200).json(results);
+  } catch (err) {
+    next(err);
+  }
+});
+
 module.exports = router;
