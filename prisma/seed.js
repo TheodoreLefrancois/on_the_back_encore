739dfa32bf180a4df75fdac1c695d26bf338dc2e
DavidDvpt <david.mosca69@gmail.com>
2021-01-13 16:35:27 +0100

    Revert "add migrations + seed tables (no media table)"
    This reverts commit a1e468af8df1d63433a37a8f6001b2d8bc2810c0.

--- prisma/seed.js
+++ prisma/seed.js
@@ -5,91 +5,9 @@ const seedDatas = async () => {
   await prisma.user.create({
     data: {
       firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
+      lastname: faker.name.lastName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
     },
   });
-
-  await prisma.roadTrip.create({
-    data: {
-      label: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-
-  await prisma.roadTrip.create({
-    data: {
-      label: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      user: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-
-  await prisma.pin.create({
-    data: {
-      long: 45.2,
-      lat: -0.25,
-      title: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      roadTrip: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-
-  await prisma.pin.create({
-    data: {
-      long: 49.6534,
-      lat: 3.0488,
-      title: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      roadTrip: {
-        connect: {
-          id: 1,
-        },
-      },
-    },
-  });
-
-  await prisma.pin.create({
-    data: {
-      long: 40.4167754,
-      lat: -3.3037902,
-      title: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      roadTrip: {
-        connect: {
-          id: 2,
-        },
-      },
-    },
-  });
-
-  await prisma.pin.create({
-    data: {
-      long: 51.509093,
-      lat: -0.094151,
-      title: faker.lorem.words(3),
-      description: faker.lorem.words(20),
-      roadTrip: {
-        connect: {
-          id: 2,
-        },
-      },
-    },
-  });
 };
-
-seedDatas();
